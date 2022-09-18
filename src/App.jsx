bd3fbc270ab20ed4da4fdd469cbdd26e41fcba9c
anilaavdylii <aa48857@ubt-uni.net>
2022-09-18 11:54:13 +0200

    declared some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,27 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from './Spinner';
-import useFetch from "./services/useFetch";
+import Products from "./Products";
+import {Routes, Route} from 'react-router-dom';
+import Detail from "./Detail";
+import Cart from './Cart';
 
 export default function App() {
   
-  const [size, setSize] = useState("");
-  const {data: products, loading, error} = useFetch("products?category=ring");
-  
 
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-
-  const filteredProducts = size ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size))) : products;
-
-  if(error) throw error;
-
-  if(loading) return <Spinner/>
 
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={(e) => {debugger; setSize(e.target.value)}}>
-              <option value="">All sizes</option>
-              <option value="5.0">5.0</option>
-              <option value="5.5">5.5</option>
-              <option value="6.0">6.0</option>
-              <option value="6.5">6.5</option>
-              <option value="7.0">7.6</option>
-              <option value="7.5">7.5</option>
-            </select>
-            { size && <h2>Found: {filteredProducts.length} items</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Routes>
+            <Route path="/" element={<h1>Welcome to Ring Store</h1>} />
+            <Route path="/:category" element={<Products/>} />
+            <Route path="/detail" element={<Detail/>} />
+            <Route path="/cart" element={<Cart/>} />
+          </Routes>
         </main>
       </div>
       <Footer />
---
+++ src/Products.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import Spinner from './Spinner';
+import useFetch from "./services/useFetch";
+import {useParams} from "react-router-dom"
+
+export default function Products() {
+  
+  const [size, setSize] = useState("");
+  const {category} = useParams();
+  const {data: products, loading, error} = useFetch("products?category=" + category);
+  
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  const filteredProducts = size ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size))) : products;
+
+  if(error) throw error;
+
+  if(loading) return <Spinner/>
+
+  return (
+    <>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{" "}
+            <select id="size" value={size} onChange={(e) => {debugger; setSize(e.target.value)}}>
+              <option value="">All sizes</option>
+              <option value="5.0">5.0</option>
+              <option value="5.5">5.5</option>
+              <option value="6.0">6.0</option>
+              <option value="6.5">6.5</option>
+              <option value="7.0">7.6</option>
+              <option value="7.5">7.5</option>
+            </select>
+            { size && <h2>Found: {filteredProducts.length} items</h2>}
+          </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
+    </>
+  );
+}
