9f39c1283e2f44ba63dd380db822f6003d4b4dcf
anilaavdylii <aa48857@ubt-uni.net>
2022-09-19 13:10:32 +0200

    added uncontrolled inputs

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef } from 'react';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
@@ -7,27 +7,32 @@ export default function useFetch(url){
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-  
+    const isMounted= useRef(false);
 
     useEffect (()=>{
+      isMounted.current = true;
         async function init (){
           try{
             const response = await fetch(baseUrl + url);
             if(response.ok){
                 const json = await response.json();
-                setData(json);
+                if(isMounted.current)  setData(json);
             }else{
                 throw response;
             }
           }
           catch(e){
-            setError(e);
+            if(isMounted.current) setError(e);
           }
           finally{
-            setLoading(false);
+            if(isMounted.current) setLoading(false);
           }
         }
         init();
+
+        return()=>{
+          isMounted.current = false;
+        }
       },[url]);
 
       return {data, error, loading};
